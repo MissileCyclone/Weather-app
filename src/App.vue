89144a51f6940e14f2b5e1618e328afe94e4c707
MissileCyclone <51222313+MissileCyclone@users.noreply.github.com>
2024-12-02 06:36:19 +0500

    update

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <WeatherDisplay />
+  </div>
+</template>
+
+<script>
+import WeatherDisplay from './components/WeatherDisplay.vue';
+
+export default {
+  components: {
+    WeatherDisplay
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin: 20px;
+}
+</style>
